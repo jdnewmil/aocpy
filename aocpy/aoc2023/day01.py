d6ee9eed5cf10d78ae195a3f46e2831db9f08f1f
Jeff Newmiller <jdnewmil@gmail.com>
2023-12-02 19:08:04 -0800

    2023 day 1

---
+++ aocpy/aoc2023/day01.py
@@ -0,0 +1,76 @@
+# day01.py
+"""Computation for 2023 day 1"""
+
+import re
+
+def find_first_digit_pos(s: str) -> int:
+    pos = 0
+    while pos < len(s) and not s[pos].isdigit():
+        pos += 1
+    return pos
+
+
+def find_last_digit_pos(s: str) -> int:
+    pos = len(s) - 1
+    while 0 < pos and not s[pos].isdigit():
+        pos -= 1
+    return pos
+
+
+def find_firstandlast_digits(s: str) -> str:
+    return (
+        s[find_first_digit_pos(s)]
+        + s[find_last_digit_pos(s)])
+
+def calc_calibration_sum(v: list[str]) -> int:
+    accum = 0
+    for s in v:
+        accum += int(find_firstandlast_digits(s))
+    return accum
+
+digits_dict = {
+    #'0': 0
+    #, 'zero': 0
+    '1': 1
+    , 'one': 1
+    , '2': 2
+    , 'two': 2
+    , '3': 3
+    , 'three': 3
+    , '4': 4
+    , 'four': 4
+    , '5': 5
+    , 'five': 5
+    , '6': 6
+    , 'six': 6
+    , '7': 7
+    , 'seven': 7
+    , '8': 8
+    , 'eight': 8
+    , '9': 9
+    , 'nine': 9}
+
+def find_firstandlast_re(
+    s: str
+    , reg: re.Pattern
+    , subpats: dict[str, int]
+) -> list[int]:
+    matches = reg.findall(s)
+    return [
+        subpats[matches[0]], subpats[matches[-1]]]
+    
+
+def find_firstandlast_pats(v: list[str], subpats: dict[str, int]) -> list[int]:
+    patlist = [k for k in subpats.keys()]
+    pat_core_pat = '(?=(' + '|'.join(patlist) + '))'
+    pat_core_re = re.compile(pat_core_pat)
+    return [
+        find_firstandlast_re(s, pat_core_re, subpats)
+        for s in v]
+
+def calc_pat_calibration_sum(v: list[str]) -> int:
+    vals = find_firstandlast_pats(v, digits_dict)
+    return sum(
+        [
+            i[0] * 10 + i[1]
+            for i in vals])
---
+++ aocpy/aoc2023/tests/day01_test.py
@@ -0,0 +1,56 @@
+# day01_test.py
+
+import pytest
+from .. import day01
+
+
+@pytest.fixture
+def testdta_01a() -> list[str]:
+    return [
+        '1abc2'
+        , 'pqr3stu8vwx'
+        , 'a1b2c3d4e5f'
+        , 'treb7uchet']
+
+@pytest.fixture
+def testdta_01b() -> list[str]:
+    return [
+        'two1nine'
+        , 'eightwothree'
+        , 'abcone2threexyz'
+        , 'xtwone3four'
+        , '4nineeightseven2'
+        , 'zoneight234'
+        , '7pqrstsixteen']
+
+def test_find_first_digit(testdta_01a):
+    ans = day01.find_first_digit_pos(testdta_01a[0])
+    assert ans == 0
+
+def test_find_last_digit(testdta_01a):
+    ans = day01.find_last_digit_pos(testdta_01a[0])
+    assert ans == 4
+
+def test_find_firstandlast_digits(testdta_01a):
+    ans = day01.find_firstandlast_digits(testdta_01a[0])
+    assert ans == '12'
+
+def test_calc_calibration_sum(testdta_01a):
+    ans = day01.calc_calibration_sum(testdta_01a)
+    assert ans == 142
+
+
+def test_calc_pat_calibration_sum(testdta_01b):
+    ans = day01.calc_pat_calibration_sum(testdta_01b)
+    assert ans == 281
+
+
+def test_calc_pat_calibration_sum2():
+    import pathlib
+    dta_dir_2023 = pathlib.Path('.') / 'data' / '2023'
+    with open(dta_dir_2023 / 'day01a.txt') as f:
+        lines = f.readlines()
+    lines = [s.strip() for s in lines]
+    ans = day01.calc_pat_calibration_sum(lines)
+    assert False
+
